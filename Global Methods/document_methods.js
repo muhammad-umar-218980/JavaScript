3cf36313a94cf84e9f34e664b5a0c242e1f76ced
muhammad-umar-218980 <muhammad.umar.siddique.1502@gmail.com>
2025-03-04 09:38:10 +0500

    document_methods added

---
+++ Global Methods/document_methods.js
@@ -0,0 +1,18 @@
+// write
+document.write("Hello World!");
+document.write("<br>");   
+document.write("Learning JavaScript");
+
+//title
+console.log(document.title); 
+document.title = "Learning Js"; 
+console.log(document.title);
+
+// URL
+console.log(document.URL);
+
+// domain
+console.log(document.domain);
+
+// last modified
+console.log("Last Modified: " + document.lastModified);
